eca64debc909dc9ed274be84056be5bd4880aeb2
Matt <915733720@qq.com>
2016-12-19 17:15:23 +0800

    add methon plus

--- Spider/MyClass.py
+++ Spider/MyClass.py
@@ -4,5 +4,15 @@ class MyClass:
     # 定义方法
     def myMethod(self):
         print 'HelloWorld' # 方法体
-mc = MyClass()# 创建对象
+    def plus(self):
+        a1 = 1
+        a2 = 2
+        a3 = 3
+        total1 = a1 + a2 + a3
+        total2 = a1 + \
+                 a2 + \
+                 a3
+        print total1,total2
+mc = MyClass() # 创建对象
 mc.myMethod() # 调用方法
+mc.plus()
